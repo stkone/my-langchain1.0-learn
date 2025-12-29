78a4abcd14334cb2f1c8b3dd98d4837bf454779f
zkm <kunming.zhu@xinjiaoyu.com>
2025-12-29 14:08:01 +0800

    init project

---
+++ common_ai/ai_variable.py
@@ -0,0 +1,15 @@
+#可用模型列表，以及获得访问模型的客户端
+#实际使用时可以根据自己的实际情况调整
+# 通义常用变量
+ALI_TONGYI_API_KEY_OS_VAR_NAME = "DASHSCOPE_API_KEY"
+ALI_TONGYI_URL = "https://dashscope.aliyuncs.com/compatible-mode/v1"
+ALI_TONGYI_MAX_MODEL = "qwen-max-latest"
+ALI_TONGYI="tongyi"
+ALI_TONGYI_DEEPSEEK_R1 = "deepseek-r1"
+ALI_TONGYI_DEEPSEEK_V3 = "deepseek-v3"
+ALI_TONGYI_REASONER_MODEL = "qvq-max-latest"
+ALI_TONGYI_EMBEDDING_MODEL = "text-embedding-v3"
+ALI_TONGYI_RERANK_MODEL = "gte-rerank-v2"
+
+
+
